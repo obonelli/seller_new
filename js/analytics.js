57d697618bf32742f2981342036ac4ab514301ef
Oscar Antonio Bonelli Nasta <oscar.bonelli@vantis.mx>
2022-01-23 21:46:45 -0600

    Primeros cambios

---
+++ js/analytics.js
@@ -0,0 +1,518 @@
+
+
+jQuery(function () {
+
+    //INICIO MENU SUPERIOR
+    $( document ).ready(function() {
+        dataLayer.push({
+            page:'/landing-page-seller',
+            title:'Landing Page Seller',
+            event:'pvLandingPageSeller'
+          });
+          
+    });
+    $("#logo").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Logo',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+    });
+    $("#navbarDropdownMenuLink").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+	// $("#anchorcuros").on('click', function () {   
+			// dataLayer.push({
+            // nd1:'Landing Page Seller',
+            // nd2:'Menú superior',
+            // nd3:'Cursos - Contraer',
+            // nd4:'',
+            // event:'clicLandingPageSellerGeneral'
+            // }); 
+			
+    // });
+	
+	$("#H6CursosMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+    $("#navbarDropdownMenuLink2").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Seminarios web',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });   
+    });
+	
+	// $("#SeminariosWebMovil").on('click', function () {  
+			// dataLayer.push({
+            // nd1:'Landing Page Seller',
+            // nd2:'Menú superior',
+            // nd3:'Seminarios web - Contraer',
+            // nd4:'',
+            // event:'clicLandingPageSellerGeneral'
+            // });
+    // });
+	
+    $("#ayuda").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Ayuda',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });  
+    });
+	
+	$("#AyudaMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Ayuda',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });  
+    });
+	
+    $("#LoginSuperior").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Iniciar sesión',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+	$("#IniciarSesionMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Iniciar sesión',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+    $("#BtnCreateAccount").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Crear mi cuenta',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+    });
+	
+	$("#btnCrearCuenta3").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Crear mi cuenta',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+    });
+	
+	$("#BtnCreateAccountM").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Crear mi cuenta',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+    });
+    //FIN MENU SUPERIOR
+
+    //INICIO SUB MENU SUPERIOR
+    $("#CrearCatalogo").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Crear catálogo',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+	$("#CrearCatalogoMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Crear catálogo',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+    $("#Ordenes").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Órdenes',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });  
+    });
+	
+	$("#OrdenesMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Órdenes',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });  
+    });
+	
+    $("#Devoluciones").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - ¿Cómo funcionan las devoluciones?',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+	$("#DevolucionesMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - ¿Cómo funcionan las devoluciones?',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+    $("#PagoSeller").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Pago a seller',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });   
+    });
+	
+	$("#PagosSellerMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Pago a seller',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });   
+    });
+	
+    $("#ConfiguracionCuenta").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Configuración de mi cuenta',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+	
+	$("#ConfiguracionCuentaMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Cursos - Configuración de mi cuenta',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });    
+    });
+ 
+
+    $("#btnCreateAccount1").on('click', function () {
+        dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Banner: Multiplica tus ventas con Coppel Marketplace',
+        nd3:'Crear mi cuenta superior',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+
+            
+    });
+    $("#VideoChica").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Video',
+            nd3:'Reproducir video',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+
+            
+    });
+    $("#Guia").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'¿Cómo funciona Coppel Marketplace?',
+            nd3:'Descarga la guía',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });      
+    });	
+    //FIN SUB MENU SUPERIOR
+
+  // INICIO SELLERS
+  $("#rowInferior").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Recursos para nuestros sellers',
+        nd3:'Cursos',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+});
+
+$("#CreateFolderInferior").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Recursos para nuestros sellers',
+        nd3:'Cursos - Crear catálogo',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+    });
+
+    $("#OrderInferior").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Recursos para nuestros sellers',
+            nd3:'Cursos - Órdenes',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+        });
+        $("#DevolucionesInferior").on('click', function () {
+            dataLayer.push({
+                nd1:'Landing Page Seller',
+                nd2:'Recursos para nuestros sellers',
+                nd3:'Cursos - ¿Cómo funcionan las devoluciones?',
+                nd4:'', 
+                event:'clicLandingPageSellerGeneral'
+                });
+            });
+            $("#PagoSellerInferior").on('click', function () {
+                dataLayer.push({
+                    nd1:'Landing Page Seller',
+                    nd2:'Recursos para nuestros sellers',
+                    nd3:'Cursos - Pago a seller',
+                    nd4:'', 
+                    event:'clicLandingPageSellerGeneral'
+                    });
+                });
+                $("#ConfiguracionInferior").on('click', function () {
+                    dataLayer.push({
+                        nd1:'Landing Page Seller',
+                        nd2:'Recursos para nuestros sellers',
+                        nd3:'Cursos - Configuración de mi cuenta',
+                        nd4:'', 
+                        event:'clicLandingPageSellerGeneral'
+                        });
+                    });
+                              
+
+//
+
+$("#SeminariosWebInferior").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Recursos para nuestros sellers',
+        nd3:'Seminarios web',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+});
+$("#CreateFolderWebInferior").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Recursos para nuestros sellers',
+        nd3:'Seminarios web - Crea tu catálogo',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+    });
+    $("#GestionaTiendaWebInferior").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Recursos para nuestros sellers',
+            nd3:'Seminarios web - Gestiona tu tienda',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });
+        });
+        $("#AumentaVentasWebInferior").on('click', function () {
+            dataLayer.push({
+                nd1:'Landing Page Seller',
+                nd2:'Recursos para nuestros sellers',
+                nd3:'Seminarios web - Aumenta tus ventas',
+                nd4:'', 
+                event:'clicLandingPageSellerGeneral'
+                });
+            });
+			
+			
+			$("#creatucatalogoSup").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Menú superior',
+        nd3:'Seminarios web - Crea tu catálogo',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+    });
+	
+	$("#CreaTuCatalogoMovil").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Menú superior',
+        nd3:'Seminarios web - Crea tu catálogo',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+    });
+	
+    $("#gestionaSup").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Seminarios web - Gestiona tu tienda',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });
+        });
+		
+		$("#GestionaTiendaMovil").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú superior',
+            nd3:'Seminarios web - Gestiona tu tienda',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });
+        });
+		
+        $("#aumentaSup").on('click', function () {
+            dataLayer.push({
+                nd1:'Landing Page Seller',
+                nd2:'Menú superior',
+                nd3:'Seminarios web - Aumenta tus ventas',
+                nd4:'', 
+                event:'clicLandingPageSellerGeneral'
+                });
+            });
+			
+			$("#AumentaVentasMovil").on('click', function () {
+            dataLayer.push({
+                nd1:'Landing Page Seller',
+                nd2:'Menú superior',
+                nd3:'Seminarios web - Aumenta tus ventas',
+                nd4:'', 
+                event:'clicLandingPageSellerGeneral'
+                });
+            });
+			
+
+  //FIN SELLERS
+  $("#btnCreateAccount2").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'¡Es hora de vender más!',
+        nd3:'Crear mi cuenta inferior',
+        nd4:'', 
+        event:'clicLandingPageSellerGeneral'
+        });
+        
+});
+//PIE DE PAGINA INICIO
+$("#AyudaPiePagina").on('click', function () {
+    dataLayer.push({
+        nd1:'Landing Page Seller',
+        nd2:'Menú inferior',
+        nd3:'Ayuda',
+        nd4:'',
+        event:'clicLandingPageSellerGeneral'
+        });
+        
+    });
+    $("#TerminosCondicionesPiePagina").on('click', function () {
+        dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'Menú inferior',
+            nd3:'Términos y condiciones',
+            nd4:'',
+            event:'clicLandingPageSellerGeneral'
+            });
+            
+        });
+        $("#AvisoPrivacidadPiePagina").on('click', function () {
+            dataLayer.push({
+                nd1:'Landing Page Seller',
+                nd2:'Menú inferior',
+                nd3:'Aviso de privacidad',
+                nd4:'',
+                event:'clicLandingPageSellerGeneral'
+                }); 
+            });
+			
+		$(".menu-2").on('click', function () {
+		dataLayer.push({
+		nd1:'Landing Page Seller',
+		nd2:'Menú Hamburguesa',
+		nd3:'Desplegar',
+		nd4:'',
+		event:'clicLandingPageSellerGeneral'
+		});
+		});
+		
+		$(".tachesito").on('click', function () {
+		dataLayer.push({
+		nd1:'Landing Page Seller',
+		nd2:'Menú Hamburguesa',
+		nd3:'Contraer',
+		nd4:'',
+		event:'clicLandingPageSellerGeneral'
+		});
+		});
+			
+//PIE DE PAGINA FIN
+});
+
+function myFunction(){
+		dataLayer.push({
+            nd1:'Landing Page Seller',
+            nd2:'¿Cómo funciona Coppel Marketplace?',
+            nd3:'Descarga la guía',
+            nd4:'', 
+            event:'clicLandingPageSellerGeneral'
+            });
+	}
---
+++ js/form.js
@@ -0,0 +1,113 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form ...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Continuar";
+    }
+    // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !validateInputs()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form...
+    if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+}
+
+function checkIfEmailInString(text) {
+    var re = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+    return re.test(text);
+}
+
+EmailModalInput = document.getElementById("correo_electronico").value;
+
+if (checkIfEmailInString(EmailModalInput) == true) {
+}
+
+function validateInputs() {
+    var els = document.getElementsByName("Bandera[]");
+    // A loop that checks every input field in the current tab:
+    for (var i = 0; i < els.length; i++) {
+        var IdActual = els[i].id;
+        var InputActual = document.getElementById(IdActual).children[1].id;
+        var InputActualValue = document.getElementById(IdActual).children[1].value;
+        if (!InputActualValue) {
+
+            if (InputActual == "correo_electronico") {
+                if (checkIfEmailInString(EmailModalInput) == true) {
+                }
+            }
+            //console.log(document.getElementById(IdActual).children[2]);
+            //document.getElementById(IdActual).children[2].className = "error_enabled";
+            currentId = document.getElementById(IdActual).children[2].id;
+            document.getElementById(currentId).className = "error_enabled";
+            valid = false;
+        } else {
+            document.getElementById(IdActual).children[2].className = "error_disabled";
+            valid = true;
+        }
+    }
+
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false:
+            valid = false;
+        }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+}
